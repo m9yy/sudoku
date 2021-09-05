83a2ec2e2cf8d040b3d30a4ad785f6ab1a7e909f
Sergey Larchenko <newm9yy2020@gmail.com>
2021-09-05 11:13:58 +0300

    feat:ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for(let i = 0; i < 9; i++){
+    for(let j = 0; j < 9; j++){
+      if(matrix[i][j] === 0){
+        for(let q = 1; q <= 9; q++){
+          if(mat(matrix, i, j, q)){
+            matrix[i][j] = q;
+            if(solveSudoku(matrix)){
+              return matrix;
+            } else {
+              matrix[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return matrix;
+}
+
+function mat (matrix, strok, stolb, num){
+  for(let index = 0; index < 9; index++){
+    let x = 3 * Math.floor(strok / 3) + Math.floor(index / 3);
+    let y = 3 * Math.floor(stolb / 3) +index % 3;
+    if(matrix[strok][index] === num || matrix [index][stolb] === num || matrix[x][y] === num){
+      return false;
+    }
+  }
+  return true;
 }
